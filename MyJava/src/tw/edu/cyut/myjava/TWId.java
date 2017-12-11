dda8bef1595d4b431f234cf201c2a8381af02909
E517 <E517@E517-58>
2017-12-11 11:49:49 +0800

    201712111149

---
+++ MyJava/src/tw/edu/cyut/myjava/TWId.java
@@ -0,0 +1,23 @@
+package tw.edu.cyut.myjava;
+
+public class TWId {
+	private String id;
+	
+	public TWId() {
+		
+	}
+	public TWId(boolean isMale) {
+		
+	}
+	public TWId(int area) {
+		
+	}
+	public TWId(boolean isMale, int area) {
+		
+	}
+	
+	private TWId(String id) {
+		this.id = id;
+	}
+	
+}
