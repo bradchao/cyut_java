057b7feab1bf93d791e0836ecd96fd3b7e29a153
E517 <E517@E517-58>
2017-12-11 10:16:21 +0800

    201712111016

--- MyJava/src/tw/edu/cyut/myjava/Cyut01.java
+++ MyJava/src/tw/edu/cyut/myjava/Cyut01.java
@@ -4,14 +4,15 @@ public class Cyut01 {
 
 	public static void main(String[] args) {
 		Bike b1 = new Bike();
-		System.out.println(b1.speed);
+		System.out.println(b1.getSpeed());
 		b1.upSpeed();
 		b1.upSpeed();
 		b1.upSpeed();
-		System.out.println(b1.speed);
+		System.out.println(b1.getSpeed());
 		b1.downSpeed();
-		System.out.println(b1.speed);
-		
+		System.out.println(b1.getSpeed());
+		//b1.speed = 100;
+		System.out.println(b1.getSpeed());
 		
 		
 	}
@@ -19,7 +20,7 @@ public class Cyut01 {
 }
 
 class Bike {
-	double speed;
+	private double speed;
 	
 	void upSpeed() {
 		speed = (speed<1)?1:speed*1.2;
@@ -29,6 +30,9 @@ class Bike {
 		speed = (speed<1)?0:speed*0.7;
 	}
 	
+	double getSpeed() {
+		return speed;
+	}
 	
 }
 
