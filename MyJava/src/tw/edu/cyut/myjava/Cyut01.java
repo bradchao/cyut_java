b6c2678ec8bbb84b784e1a6c08c8d2ba8517e62c
E517 <E517@E517-58>
2017-12-11 10:01:11 +0800

    201712111001

---
+++ MyJava/src/tw/edu/cyut/myjava/Cyut01.java
@@ -0,0 +1,31 @@
+package tw.edu.cyut.myjava;
+
+public class Cyut01 {
+
+	public static void main(String[] args) {
+		Bike b1 = new Bike();
+		System.out.println(b1.speed);
+		b1.upSpeed();
+		b1.upSpeed();
+		b1.upSpeed();
+		System.out.println(b1.speed);
+		
+		
+	}
+
+}
+
+class Bike {
+	double speed;
+	
+	void upSpeed() {
+		speed = (speed<1)?1:speed*1.2;
+	}
+	
+	void downSpeed() {
+		speed = (speed<1)?0:speed*0.7;
+	}
+	
+	
+}
+
